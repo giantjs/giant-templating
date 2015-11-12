32f2a25168854dc7ee04b1a7ecf66fe677da5f4e
Dan Stocker <dan@kwaia.com>
2015-11-12 10:24:56 +0000

    replaces test browser with PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -11,6 +11,8 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
+            'node_modules/phantomjs-function-bind-polyfill/index.js',
+
             'node_modules/giant-assertion/lib/giant-assertion.js',
             'node_modules/giant-oop/lib/giant-oop.js',
             'config/giant-oop-test.config.js',
@@ -20,7 +22,6 @@ module.exports = function (config) {
             'node_modules/giant-event/lib/giant-event.js',
 
             'src/namespace.js',
-
             'src/**/*.def.js',
             'src/**/*.test.js'
         ],
@@ -52,7 +53,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
